44a1186b2877a1cf8ab6f52fbdcebd4c86589a8a
NemoBJJ <engnfaraujo@gmail.com>
2026-06-13 14:55:33 -0300

    refactor: update api.js with Render endpoint and env logic

--- src/api.js
+++ src/api.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
+const BASE_URL = isDevelopment
+  ? 'http://localhost:8084/api'
+  : 'https://servicos-api-v7rj.onrender.com/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:8084/api',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
