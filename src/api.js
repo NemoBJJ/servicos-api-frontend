c9f814b8832e371908622d4b5c1894bf773d7876
NemoBJJ <engnfaraujo@gmail.com>
2026-07-30 15:35:22 -0300

    Migra backend de Serviços para VPS Contabo

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const isDevelopment =
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1';
 
 const BASE_URL = isDevelopment
   ? 'http://localhost:8084/api'
-  : 'https://servicos-api-v7rj.onrender.com/api';
+  : 'https://servicos-container.neemindev.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
